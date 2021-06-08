cd173ec6c9a1a231dc87b04e3ccc712d2aac9c85
Scot Hampel <scot@scothampel.com>
2021-06-08 16:57:44 -0500

    Bootstrap Bundle

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
